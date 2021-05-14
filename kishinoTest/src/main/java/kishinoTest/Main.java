a9c9dc90c637601c601a800360f05e2c4372b7b2
tom-kish <tomoyuki.kishino@toppan.co.jp>
2021-05-14 16:08:43 +0900

    dekita

--- kishinoTest/src/main/java/kishinoTest/Main.java
+++ kishinoTest/src/main/java/kishinoTest/Main.java
@@ -1,62 +1,90 @@
-package kishinoTest;
+package jp.co.toppan;
 
+import java.util.Calendar;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudfront.CloudFrontClient;
 import software.amazon.awssdk.services.cloudfront.model.CreateInvalidationRequest;
 import software.amazon.awssdk.services.cloudfront.model.InvalidationBatch;
+import software.amazon.awssdk.services.cloudfront.model.ListDistributionsResponse;
+import software.amazon.awssdk.services.cloudfront.model.Paths;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world!");
+			System.out.println("hello world!");
 
-//		// S3に接続
-//		AmazonS3 client = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_1).build();;
+
+			// S3に接続
+//			AmazonS3 client = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_1).build();
+
+			// S3へアップロード
+//			ByteArrayInputStream bis = new ByteArrayInputStream(naiyo.getBytes());
 //
-//		// S3へアップロード
-//		String naiyo = "どうしよう";
+//			ObjectMetadata metaData = new ObjectMetadata();
+//			metaData.setContentLength(naiyo.getBytes().length);
 //
-//		ByteArrayInputStream bis = new ByteArrayInputStream(naiyo.getBytes());
-//
-//		ObjectMetadata metaData = new ObjectMetadata();
-//
-		try {
-//			client.putObject("salad-bar-dev-static-files",  "TOPPAN/9999.txt", bis, metaData);
-
-//			AWSCredentials awsCredentials = new DefaultAWSCredentialsProviderChain().getCredentials();
-			CloudFrontClient client = CloudFrontClient.create();
-
-//			Paths invalidation_paths = new Paths().withItems("/path/to/invalidate/foo.jpg", "/path/file2.txt").withQuantity(2);
-//			InvalidationBatch invalidation_batch = new InvalidationBatch(invalidation_paths, "unique_id_like_a_date");
-//			CreateInvalidationRequest invalidation = new CreateInvalidationRequest("distributionID", invalidation_batch);
-//			CreateInvalidationResult ret = client.createInvalidation(invalidation);
+			try {
+//				client.putObject("salad-bar-dev-static-files",  "TOPPAN/9999.txt", bis, metaData);
 
 
-			CloudFrontClient amazonCloudFront = CloudFrontClient.create();
-//			AmazonCloudFront amazonCloudFront = AmazonCloudFrontClientBuilder.standard().build();
-			software.amazon.awssdk.services.cloudfront.model.Paths paths = software.amazon.awssdk.services.cloudfront.model.Paths.builder()
-					.items("/TOPPAN/18")
-					.quantity(1)
-					.build();
-			//		new Paths().withItems("/path/*");
+				CloudFrontClient amazonCloudFront = CloudFrontClient.builder()
+						.region(Region.AWS_GLOBAL)
+						.build();
 
-			InvalidationBatch invalidationBatch = InvalidationBatch.builder().paths(paths).build();
+				Paths paths = Paths.builder()
+						.items("/TOPPAN/18")
+						.quantity(1)
+						.build();
 
+				InvalidationBatch invalidationBatch = InvalidationBatch.builder()
+						.paths(paths)
+						.callerReference(Long.toString(Calendar.getInstance().getTimeInMillis()))
+						.build();
 
-			// InvalidationBatch invalidationBatch = new InvalidationBatch(paths, Long.toString(Calendar.getInstance().getTimeInMillis()));
-			CreateInvalidationRequest request = CreateInvalidationRequest
+				ListDistributionsResponse res = amazonCloudFront.listDistributions();
+
+				res.distributionList().items().forEach(item -> {
+					System.out.println(item.id());
+					CreateInvalidationRequest request = CreateInvalidationRequest
 					.builder()
-					.distributionId("<DISTRIBUTION_ID>").
-					invalidationBatch(invalidationBatch).build();
+					.distributionId(item.id())
+					.invalidationBatch(invalidationBatch).build();
 
-			amazonCloudFront.createInvalidation(request);
+					amazonCloudFront.createInvalidation(request);
+				});
 
 
-		} catch (Exception e) {
-			e.printStackTrace();
+
+
+
+				String backetName = "salad-bar-dev-static-files";
+				String fileName = "18";
+				String naiyo = "アクセス設定確認４";
+
+				S3Client s3Client = S3Client.builder().region(Region.AP_NORTHEAST_1).build();
+
+				s3Client.putObject(PutObjectRequest.builder()
+		                .bucket(backetName)
+		                .key("TEST/" + fileName)
+//		                .acl(ObjectCannedACL.PUBLIC_READ)
+		                .build(),
+		                RequestBody.fromBytes(naiyo.getBytes()));
+//
+//				String mattaku = s3Client.utilities().getUrl(builder -> builder.bucket(backetName).key("TEST/" + fileName)).toExternalForm();
+//
+//				System.out.println(mattaku);
+
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			System.out.println("end");
 		}
 
-
-		System.out.println("end");
-	}
 
 }
